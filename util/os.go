0b1eda939347842695153a37496c2e49e3b5ae52
Sting Alleman <stingalleman@icloud.com>
2021-05-02 11:27:06 +0100

    feat(util/os): add CreateTempFolder method

--- util/os.go
+++ util/os.go
@@ -1,9 +1,28 @@
 package util
 
-import "path"
+import (
+	"os"
+	"path"
+)
 
 var (
-	TmpPath    = "/var/tmp/stew"
+	TmpRootPath = "/var/tmp/stew"
+	TmpPath     string
+	TmpSrcPath  string
+	TmpPkgPath  string
+	Gid         string
+	Uid         string
+)
+
+func CreateTempFolder() (tmpPath string, err error) {
+	os.MkdirAll(TmpRootPath, 0774)
+
+	tmpPath, err = os.MkdirTemp(TmpRootPath, "pkg")
+	TmpPath = tmpPath
 	TmpSrcPath = path.Join(TmpPath, "src")
 	TmpPkgPath = path.Join(TmpPath, "package")
-)
+
+	os.Mkdir(TmpPkgPath, 0774)
+
+	return
+}
