cffef16f4cf9e2454ab8e87f081f2925c9259098
Sting Alleman <stingalleman@icloud.com>
2021-07-16 02:43:24 +0200

    feat: use grab to download

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/stewproject/builder
 go 1.16
 
 require (
+	github.com/cavaliercoder/grab v2.0.0+incompatible // indirect
 	github.com/mholt/archiver/v3 v3.5.0
 	github.com/pelletier/go-toml v1.9.0 // indirect
 	github.com/spf13/cobra v1.1.3
--- util/download.go
+++ util/download.go
@@ -1,47 +1,25 @@
 package util
 
 import (
-	"io"
-	"mime"
-	"net/http"
 	"os"
 	"path"
 
+	"github.com/cavaliercoder/grab"
 	"github.com/mholt/archiver/v3"
 )
 
 // Download archive from URL & decompress
 func DownloadArchive(url string, outPath string) error {
 	// Get the data
-	resp, err := http.Get(url)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-
-	_, params, err := mime.ParseMediaType(resp.Header.Get("Content-Disposition"))
-	if err != nil {
-		return err
-	}
-	tarName := params["filename"]
-
-	// Create the file
-	out, err := os.Create(path.Join(TmpPath, tarName))
-	if err != nil {
-		return err
-	}
-	defer out.Close()
-
-	// Write the body to file
-	_, err = io.Copy(out, resp.Body)
+	resp, err := grab.Get(path.Join(TmpPath), url)
 	if err != nil {
 		return err
 	}
 
-	err = archiver.Unarchive(out.Name(), TmpSrcPath)
+	err = archiver.Unarchive(resp.Filename, TmpSrcPath)
 	if err != nil {
 		return err
 	}
 
-	return os.Remove(out.Name())
+	return os.Remove(resp.Filename)
 }
